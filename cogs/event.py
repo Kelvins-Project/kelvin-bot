f582eeec81d216c58b25d105b3e6f1bcf1f0aab3
Yonipy <itz.kqlvin@gmail.com>
2023-04-16 22:03:23 -0400

    ddddd

--- cogs/event.py
+++ cogs/event.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 import traceback
 import sys
 import discord
-import datetime
 class Event(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,36 +13,27 @@ class Event(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, before : discord.Message, after : discord.Message):
-        if before.content != after.content:
-            ctx = await self.bot.get_context(after)
-            await self.bot.invoke(ctx)
-        channel = self.bot.get_channel(1097214136460517476)
-        await channel.send(f'{after.author} editted "{before.content}" to "{after.content}" in {after.channel.mention}')
-    
-
-    @commands.Cog.listener()
-    async def on_message(self, message : discord.Message):
-        if message.channel.id != 1097200149035483238:
-            return 
-        if message.content.lower() != "verify me":
-            await message.delete()
-        else:
-            logs = self.bot.get_channel(1097214136460517476)
-            channel = self.bot.get_channel(1097198382969274399)
-            await message.delete()
-            await message.author.add_roles(message.guild.get_role(1097199833917444237))
-            await channel.send(f'{message.author.mention} welcome to the server!')
-            await logs.send(f'{message.author} verified themselves')
+        #if before.content != after.content:
+        #    ctx = await self.bot.get_context(after)
+        #    await self.bot.invoke(ctx)
+        channel = self.bot.get_channel(1097334267060682873)
+        embed = discord.Embed(title="Message Edited", description=f"**Before:** ``{before.content}``\n**After:** ``{after.content}``", color=0x2F3136)
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message : discord.Message):
-        channel = self.bot.get_channel(1097214136460517476)
-        await channel.send(f'{message.author} deleted "{message.content}" in {message.channel.mention}') 
+        channel = self.bot.get_channel(1097334267060682873)
+        embed = discord.Embed(title="Message Deleted", description=f"Message by {message.author}: ``{message.content}``", color=0x2F3136)
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = self.bot.get_channel(1097214136460517476)
-        await channel.send(f'member {member} joined the server')
+        log = self.bot.get_channel(1097334267060682873)
+        channel = self.bot.get_channel(1097198382969274399)
+        embed = discord.Embed(description=f"welc {member.mention}\nenjoy your stay", color=0x2F3136)
+        loge = discord.Embed(title="Member Joined", description=f"{member.mention} has joined the server.", color=0x2F3136)
+        await log.send(embed=loge)
+        await channel.send(embed=embed)
         
     
         
