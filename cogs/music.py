a14b1f07ffc1e191ea6e25e42fbd97033943396c
Yonipy <itz.kqlvin@gmail.com>
2023-04-20 23:40:58 -0400

    eeeeee

--- cogs/music.py
+++ cogs/music.py
@@ -25,7 +25,7 @@ ytdlopts = {
 }
 
 ffmpegopts = {
-    'before_options': '-nostdin',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -nostdin ',
     'options': '-vn'
 }
 
