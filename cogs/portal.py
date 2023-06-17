910cc3f9455835a492cd30fdf1f76ea98164f8af
Yonipy <itz.kqlvin@gmail.com>
2023-06-17 13:12:21 -0400

    eeeeeeee

--- cogs/portal.py
+++ cogs/portal.py
@@ -111,3 +111,16 @@ class Portal(commands.Cog):
             await ctx.author.add_roles(role)
             await ctx.send(f'you have been given access to {access.lower()} servers')
         
+    @commands.Cog.listener('on_message')
+    async def ad_divider(self, message):
+        embed = discord.Embed()
+        embed.set_image(url='https://64.media.tumblr.com/3eeb3876b5e82cde2e742d917965cda9/aeec771f2613a911-b6/s2048x3072/35d14bb1b40d524d14d64d8b81da47597952fb64.pnj')
+        if message.author.id == self.bot.user.id:
+            return
+        if message.author.bot:
+            return
+        if message.channel.id != 1098228193456038008:
+            return
+        match = re.findall(r'(?:https?://)?discord(?:app)?\.(?:com/invite|gg)/[a-zA-Z0-9]+/?', message.content)
+        if match:
+            await message.channel.send(embed=embed, color=0x2F3136)
