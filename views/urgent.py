2cd85e2bdf47cf7377be3249aeaec7f8b9382e66
Yonipy <itz.kqlvin@gmail.com>
2023-06-20 12:56:36 -0400

    eeeeeeeeee

--- views/urgent.py
+++ views/urgent.py
@@ -15,9 +15,9 @@ class Urgent(discord.ui.Select):
         super().__init__(placeholder='choose a option', min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction):
-        await interaction.message.delete()
         channel_name = interaction.channel.name
-        pin = await interaction.channel.send(f'you have selected **{self.values[0]}**')
+        await interaction.response.defer()
+        pin = await interaction.followup.send(f'you have selected **{self.values[0]}**')
         await pin.pin()
         await interaction.channel.edit(name=f'{channel_name}-{self.values[0]}')
 
