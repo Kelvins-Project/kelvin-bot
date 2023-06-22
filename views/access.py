b923c3f31ab188931bdceff95b2f58792907137b
Yonipy <itz.kqlvin@gmail.com>
2023-06-22 11:33:36 -0400

    eeeeeeeeeeee

--- views/access.py
+++ views/access.py
@@ -3,46 +3,41 @@ import discord
 class AccessView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        self.dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
 
     @discord.ui.button(label='stox', style=discord.ButtonStyle.green, custom_id='persistent_view:stox')
     async def stox(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        role = interaction.guild.get_role(dir['stox'])
+        role = interaction.guild.get_role(self.dir['stox'])
         await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to stox servers')
     
     @discord.ui.button(label='icon/decor', style=discord.ButtonStyle.green, custom_id='persistent_view:icon_decor')
     async def icon_decor(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        role = interaction.guild.get_role(dir['icon/decor'])
+        role = interaction.guild.get_role(self.dir['icon/decor'])
         await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to icon/decor servers')
 
     @discord.ui.button(label='social', style=discord.ButtonStyle.green, custom_id='persistent_view:social')
     async def social(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        role = interaction.guild.get_role(dir['social'])
+        role = interaction.guild.get_role(self.dir['social'])
         await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to social servers')
 
     @discord.ui.button(label='other', style=discord.ButtonStyle.green, custom_id='persistent_view:other')
     async def other(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        role = interaction.guild.get_role(dir['other'])
+        role = interaction.guild.get_role(self.dir['other'])
         await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to other servers')
 
     @discord.ui.button(label='mnml', style=discord.ButtonStyle.green, custom_id='persistent_view:mnml')
     async def mnml(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        role = interaction.guild.get_role(dir['mnml'])
+        role = interaction.guild.get_role(self.dir['mnml'])
         await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to mnml servers')
 
     @discord.ui.button(label='all', style=discord.ButtonStyle.green, custom_id='persistent_view:all')
     async def all(self, interaction: discord.Interaction, button: discord.ui.Button):
-        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
-        for ids in dir.values():
+        for ids in self.dir.values():
                 role = interaction.guild.get_role(ids)
                 await interaction.user.add_roles(role)
         await interaction.response.send_message(f'you have been given access to all servers')
--- views/done.py
+++ views/done.py
@@ -4,7 +4,6 @@ from views.urgent import UrgentView
 class Done(discord.ui.Select):
     def __init__(self):
 
-    
         options = [
             discord.SelectOption(label='batch', description='batching ads'),
             discord.SelectOption(label='sep-1h', description='seps ads for 1h'),
@@ -15,9 +14,14 @@ class Done(discord.ui.Select):
         super().__init__(placeholder='choose a option', min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction: discord.Interaction):
+        dir = {'stox': 1105116553122435112, 'icon/decor': 1105116582730006578, 'social': 1105116620445192204, 'mnml': 1105463864012845166, 'other': 1105150560795111446}
         await interaction.response.defer()
         await interaction.followup.send(f'you have selected **{self.values[0]}**')
         await interaction.channel.send(f'{interaction.user.mention} select a urgentness', view=UrgentView(self.values[0]))
+        for ids in dir.values():
+            role = interaction.guild.get_role(ids)
+            await interaction.user.remove_roles(role)
+        
 
 class DoneView(discord.ui.View):
     def __init__(self):
